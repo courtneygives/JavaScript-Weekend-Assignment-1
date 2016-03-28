7e065c36067698f4eb506944caa6f8451eedf70c
Courtney Ives <cgives@me.com>
2016-03-27 22:32:41 -0500

    complete challenge

--- app.js
+++ app.js
@@ -30,14 +30,54 @@ var claim5 = {
 
 var initialList = [claim1, claim2, claim3, claim4, claim5]
 
-var totalPayedOut = 0;
+var totalPayout = 0;
 
-function claim(name, type, cost){
+function Claim (name, type, cost){
 	this.patientName = name;
 	this.visitType = type;
 	this.visitCost = cost;
+	this.payout = 0;
+
 }
 
-//function to determine percent covered
+var moList = [
+	new Claim("Genghis Khan", "Emergency", 11000),
+	new Claim("Copernicus", "Optical", 980),
+	new Claim("Taylor Swift", "Primary Care", 400),
+	new Claim("George Washington", "Specialist", 6900),
+	new Claim("Henry Tudor VIII", "Primary Care", 30)
+];
 
-//function to determine amount covered
+var finalList = initialList.concat(moList);
+
+//function to determine percent covered
+function percentage (claimObject){
+	switch (claimObject.visitType) {
+		case "Optical":
+			claimObject.payout = claimObject.visitCost * 0;
+			break;
+		case "Specialist":
+			claimObject.payout = claimObject.visitCost * .10;
+			break;
+		case "Emergency":
+			claimObject.payout = claimObject.visitCost * 1;
+			break;
+		case "Primary Care":
+			claimObject.payout = claimObject.visitCost * .5;
+			break;
+		default:
+			claimObject.payout = claimObject.visitCost * 0;
+	}
+	console.log(claimObject.patientName + " payout: $" + claimObject.payout);
+	totalPayout += claimObject.payout;
+}
+
+//function to determine amount covered and log payout message
+function coverage (){
+	for (it = 0; it < finalList.length; it++){
+		percentage(finalList[it]);
+		console.log('Paid out $' + finalList[it].payout + ' for ' + finalList[it].patientName + ", making the total payouts $" + totalPayout);
+	}
+}
+
+coverage();
